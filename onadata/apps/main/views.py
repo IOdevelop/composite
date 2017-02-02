248b35e0965601492966a18b4ea4b8827de89052
nebula <francisbnagura@gmail.com>
2017-02-02 11:51:50 +0000

    Lets rollback again

--- onadata/apps/main/views.py
+++ onadata/apps/main/views.py
@@ -187,7 +187,7 @@ def profile(request, username):
                     'id_string': survey.id_string
                 }),
                 'text': _(u'Successfully published %(form_id)s. Click on the Projects link above to get started'
-                        #  u' <a href="%(form_url)s">Enter Web Form</a>'
+                        u' <a href="%(form_url)s">Enter Web Form</a>'
                          # u' or <a href="#preview-modal" data-toggle="modal">'
                          # u'Preview Web Form</a>'
                           )
