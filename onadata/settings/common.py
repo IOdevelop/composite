e5a6161eb35629b7c54fc9240ff04281f978f88c
butagreeza <francisbnagura@gmail.com>
2019-03-15 13:41:12 +0000

    Added Bla

--- onadata/settings/common.py
+++ onadata/settings/common.py
@@ -72,7 +72,7 @@ USE_L10N = True
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = 'http://127.0.0.1:9000/media/'
+MEDIA_URL = 'http://127.0.0.1:8000/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -82,7 +82,7 @@ STATIC_ROOT = os.path.join(ONADATA_DIR, 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+
 
 # Enketo URL.
 # Configurable settings.
@@ -541,7 +541,7 @@ DATABASES = {
 
 
 SECRET_KEY = 'mlfs33^s1l4xf6a36$0#j%dd*sisfoi&)&4s-v=91#^l01v)*jio'
-ALLOWED_HOSTS = ['35.231.244.231/','127.0.0.1','localhost']
+ALLOWED_HOSTS = ['35.231.244.231','127.0.0.1','localhost']
 DEBUG=True
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
